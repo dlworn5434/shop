3cb2ae630d851d8672552c5e0b806df1960e8706
dlworn5434 <dlworn0216@gmail.com>
2024-07-31 19:11:12 +0900

    관심상풍 등록 API

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -2,8 +2,11 @@ package com.sparta.myselectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MyselectshopApplication {
 
 	public static void main(String[] args) {
